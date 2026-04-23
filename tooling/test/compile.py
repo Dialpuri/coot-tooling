1f44599d2ef9a07b96f992d33d83773b6f543215
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2026-04-23 10:14:45 +0100

    Enhanced test compilation script: added library includes, linker flags, and updated `make_test_compile_cmd` logic.

--- tooling/test/compile.py
+++ tooling/test/compile.py
@@ -16,21 +16,39 @@ GTEST_LIB_DIR = "/opt/homebrew/lib"
 
 MAX_COMPILE_ATTEMPTS = 3
 
+CXX           = "c++"
+COOT_API_DIR  = "/Users/dialpuri/lmb/build-coot-and-deps"
+COOT_API_NAME = "cootapi"
+MMDB_API_DIR  = "/opt/homebrew/Cellar/mmdb2/2.0.22/lib"
+MMDB_API_NAME = "mmdb2"
+CLIPPER_API_DIR  = "/opt/homebrew/Cellar/clipper4coot/2.1.20180802_3/lib"
+GEMMI_INCLUDE = "/opt/homebrew/opt/gemmi/include"
+CLIPPER_INCLUDE = "/opt/homebrew/Cellar/clipper4coot/2.1.20180802_3/include"
+BOOST_INCLUDE = "/opt/homebrew/Cellar/boost/1.90.0_1/include"
+MMDB_INCLUDE  = "/opt/homebrew/Cellar/mmdb2/2.0.22/include"
+GSL_INCLUDE   = "/opt/homebrew/Cellar/gsl/2.8/include"
+PNG_INCLUDE = "/opt/homebrew/Cellar/libpng/1.6.56/include"
+GLM_INCLUDE = "/opt/homebrew/Cellar/glm/1.0.1/include"
 
 def make_test_compile_cmd(test_cc: Path, output_bin: Path) -> str:
-    includes = [
-        PROJECT_ROOT, GEMMI_INCLUDE, CLIPPER_INCLUDE, BOOST_INCLUDE,
-        MMDB_INCLUDE, GSL_INCLUDE, PNG_INCLUDE, GLM_INCLUDE, GTEST_INCLUDE,
+    includes = [PROJECT_ROOT, GEMMI_INCLUDE, CLIPPER_INCLUDE, BOOST_INCLUDE, MMDB_INCLUDE, GSL_INCLUDE, PNG_INCLUDE, GLM_INCLUDE, GTEST_INCLUDE]
+    includes = " ".join(f'-I"{i}"' for i in includes)
+
+    clipper_libraries = [
+        "clipper-ccp4", "clipper-core", "clipper-cif", "clipper-cns", "clipper-contrib", "clipper-minimol", "clipper-mmdb",
+        "clipper-phs"
     ]
-    inc_flags = " ".join(f'-I"{i}"' for i in includes)
+    clipper_libraries = " ".join(f'-l{l}' for l in clipper_libraries)
+
     return (
         f'{CXX} -std=c++17 "{test_cc.absolute()}" -o "{output_bin.absolute()}" '
-        f'{inc_flags} '
+        f'{includes} '
         f'-pthread '
         f'-Wl,-rpath,{COOT_API_DIR} '
         f'-L "{COOT_API_DIR}" -l{COOT_API_NAME} '
         f'-L "{MMDB_API_DIR}" -l{MMDB_API_NAME} '
-        f'-L "{GTEST_LIB_DIR}" -lgtest -lgtest_main'
+        f'-L "{CLIPPER_API_DIR}" {clipper_libraries} '
+        f'-L "{GTEST_LIB_DIR}" -lgtest -lgtest_main '
     )
 
 
